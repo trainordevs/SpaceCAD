6b34ccc4a9717bfcf9d8f955e8c6af5b9447cbec
Robert Trainor Jr <trainordevelopments@outlook.com>
2021-09-04 00:41:00 -0500

    Added routes for creating a civilian and vehicle.

--- routes.php
+++ routes.php
@@ -1,4 +1,12 @@
 <?php
+    Route::set('civilian/create', function() {
+        Home::createCivilian();
+    });
+
+    Route::set('vehicle/create', function() {
+        Home::createVehicle();
+    });
+
     Route::set('about', function() {
         About::CreateView('about');
     });
