ea50f1386c7e88762450059e03bc35fa5c17df62
Robert Trainor Jr <trainordevelopments@outlook.com>
2021-09-03 23:23:37 -0500

    Created master config class.

---
+++ classes/Config.php
@@ -0,0 +1,11 @@
+<?php
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'spacecad');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+
+    define('TABLE_CIVILIANS', 'civilians');
+    define('TABLE_VEHICLES', 'vehicles');
+    define('TABLE_FIREARMS', 'firearms');
+    define('TABLE_USERS', 'users');
+    define('TABLE_BOLOS', 'bolos');
