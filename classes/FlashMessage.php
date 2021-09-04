3aaf5f7c812f6da240750260e9a6c98bcad48f3b
Robert Trainor Jr <trainordevelopments@outlook.com>
2021-09-04 00:41:25 -0500

    Added potential redirecting to flash messages.

--- classes/FlashMessage.php
+++ classes/FlashMessage.php
@@ -1,18 +1,30 @@
 <?php
     class FlashMessage {
-        public static function setInfo(string $message) {
+        public static function setInfo(string $message, $redirect = null) {
             $_SESSION['flash_status'] = 'info';
             $_SESSION['flash_message'] = $message;
+
+            if(!is_null($redirect)) {
+                header("Location: $redirect");
+            }
         }
 
-        public static function setError(string $message) {
+        public static function setError(string $message, $redirect = null) {
             $_SESSION['flash_status'] = 'error';
             $_SESSION['flash_message'] = $message;
+
+            if(!is_null($redirect)) {
+                header("Location: $redirect");
+            }
         }
 
-        public static function setSuccess(string $message) {
+        public static function setSuccess(string $message, $redirect = null) {
             $_SESSION['flash_status'] = 'success';
             $_SESSION['flash_message'] = $message;
+
+            if(!is_null($redirect)) {
+                header("Location: $redirect");
+            }
         }
 
         public static function clearMessage() {
