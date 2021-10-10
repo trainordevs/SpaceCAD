6caa9b48667010c9d7df9d389cb475b8bc9d1ab3
Robert Trainor Jr <trainordevelopments@outlook.com>
2021-10-09 23:53:39 -0500

    Alter config.

--- classes/Config.php
+++ classes/Config.php
@@ -1,11 +1,26 @@
 <?php
     define('DB_HOST', 'localhost');
-    define('DB_NAME', 'spacecad');
+    define('DB_NAME', 'zap-hostingesxpack_622951');
     define('DB_USER', 'root');
     define('DB_PASS', '');
 
-    define('TABLE_CIVILIANS', 'civilians');
-    define('TABLE_VEHICLES', 'vehicles');
+    define('ENABLED_FIREARMS', false);
+    define('ENABLED_CIVILIANS', true);
+    define('ENABLED_VEHICLES', true);
+
+    define('TABLE_CIVILIANS', 'users');
+    define('TABLE_VEHICLES', 'owned_vehicles');
     define('TABLE_FIREARMS', 'firearms');
-    define('TABLE_USERS', 'users');
-    define('TABLE_BOLOS', 'bolos');
+    define('TABLE_USERS', 'police_members');
+    define('TABLE_BOLOS', 'spacecad_bolos');
+
+    define('SLOGANS', [
+        "Homicide: Our day starts when yours ends.",
+        "Our Colors Never Run.",
+        "One Family. One Fight.",
+        "P.I.G.: Pride, Integrity, Guts.",
+        "Live with Honor, Serve with Pride.",
+        "To Protect and Serve.",
+        "Proud to Serve.",
+        "Justice for All."
+    ]);
