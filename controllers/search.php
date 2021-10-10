73b16dce2e782534ffc56135ee27c35f8247bf7c
Robert Trainor Jr <trainordevelopments@outlook.com>
2021-10-09 23:52:32 -0500

    Alter civilian search. Add vehicle search.

--- controllers/search.php
+++ controllers/search.php
@@ -38,13 +38,19 @@
         }
 
         public static function searchCivilians() {
-            $civilians = Civilian::getLike(['fullname' => $_POST['search']]);
+            $name = explode(" ", $_POST['search']);
+
+            if(count($name) == 1) {
+                $civilians = Civilian::getLike(['firstname' => $name[0]]);
+            } else {
+                $civilians = Civilian::getLike(['firstname' => $name[0], 'lastname' => $name[1]]);
+            }
 
             $result = "<p class='text-center'><strong>Civilians</strong></p>";
 
             if(!empty($civilians)) {
                 foreach ($civilians as $civilian) {
-                    $result .= "<p><button class='d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm w-100'>" . $civilian['fullname'] . " (DOB: " . $civilian['dob'] . ")</button></p>";
+                    $result .= "<p><button class='d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm w-100'>" . $civilian['firstname'] . " " . $civilian['lastname'] . " (DOB: " . $civilian['dateofbirth'] . ")</button></p>";
                 }
             } else {
                 $result .= "<p>No civilians match that search query.</p>";
@@ -55,12 +61,20 @@
 
         public static function searchVehicles() {
             $vehicles = Vehicle::getLike(['plate' => $_POST['search']]);
+            $obj = json_decode(file_get_contents('vehicles.json'), true);
 
             $result = "<p class='text-center'><strong>Vehicles</strong></p>";
 
             if(!empty($vehicles)) {
                 foreach ($vehicles as $vehicle) {
-                    $result .= "<p><button class='d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm w-100'>" . $vehicle['plate'] . " (" . $vehicle['make'] . " " . $vehicle['model'] . ")</button></p>";
+                    $owner = Civilian::getOne(['identifier' => $vehicle['owner']]);
+                    $model_code = json_decode($vehicle['vehicle'], true)['model'];
+
+                    foreach ($obj as $v) {
+                        if($v['Code'] == $model_code) {
+                            $result .= "<p><button class='d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm w-100'>" . $vehicle['plate'] . " (" . $v['Name'] . " - " . $owner->getFirstName() . " " . $owner->getLastName() .")</button></p>";
+                        }
+                    }
                 }
             } else {
                 $result .= "<p>No vehicles match that search query.</p>";
