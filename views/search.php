4be5ac0518b0f8495a3bec9558ade490420ad009
Robert Trainor Jr <trainordevelopments@outlook.com>
2021-10-09 23:53:31 -0500

    Alter searching.

--- views/search.php
+++ views/search.php
@@ -2,26 +2,34 @@
     <div class="col-12 mb-4">
         <div class="card shadow h-100 py-2 text-center">
             <div class="card-body">
-                <p class="text-center">You may search for a civilian by full name, vehicle by plate, or firearm by serial number.</p>
+                <p class="text-center">You may search for a civilian by full name, vehicle by plate, or firearm by
+                    serial number.</p>
                 <form class="user">
                     <div class="form-group">
-                        <input type="text" class="form-control" id="search" name="search" placeholder="Name/Plate/Serial">
+                        <input type="text" class="form-control" id="search" name="search"
+                            placeholder="Name/Plate/Serial">
                     </div>
                 </form>
                 <br>
                 <div class='row'>
+                    <?php if (ENABLED_FIREARMS == true): ?>
                     <div class='col' id='firearms'>
                         <p class='text-center'><strong>Firearms</strong></p>
                         <p>No firearms match that search query.</p>
                     </div>
+                    <?php endif; ?>
+                    <?php if (ENABLED_CIVILIANS == true): ?>
                     <div class='col' id='civilians'>
                         <p class='text-center'><strong>Civilians</strong></p>
                         <p>No civilians match that search query.</p>
                     </div>
+                    <?php endif; ?>
+                    <?php if (ENABLED_VEHICLES == true): ?>
                     <div class='col' id='vehicles'>
                         <p class='text-center'><strong>Vehicles</strong></p>
                         <p>No vehicles match that search query.</p>
                     </div>
+                    <?php endif; ?>
                 </div>
             </div>
         </div>
@@ -30,33 +38,39 @@
 
 <script>
 $("#search").keyup(function() {
-    if(!$("#search").val()) {
+    if (!$("#search").val()) {
         $("#searchResults").html("");
     } else {
-        $.ajax({    
+        $.ajax({
             type: "POST",
-            url: "/search/firearms",             
+            url: "/search/firearms",
             dataType: "html",
-            data: { search: $("#search").val() },
-            success: function(data){
+            data: {
+                search: $("#search").val()
+            },
+            success: function(data) {
                 $("#firearms").html(data);
             }
         });
-        $.ajax({    
+        $.ajax({
             type: "POST",
-            url: "/search/civilians",             
+            url: "/search/civilians",
             dataType: "html",
-            data: { search: $("#search").val() },
-            success: function(data){
+            data: {
+                search: $("#search").val()
+            },
+            success: function(data) {
                 $("#civilians").html(data);
             }
         });
-        $.ajax({    
+        $.ajax({
             type: "POST",
-            url: "/search/vehicles",             
+            url: "/search/vehicles",
             dataType: "html",
-            data: { search: $("#search").val() },
-            success: function(data){
+            data: {
+                search: $("#search").val()
+            },
+            success: function(data) {
                 $("#vehicles").html(data);
             }
         });
