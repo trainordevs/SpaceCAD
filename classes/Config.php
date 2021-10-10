3ad2441df6a0b39152e3018f706b519bec5bdf7b
Robert Trainor Jr <trainordevelopments@outlook.com>
2021-10-10 00:04:48 -0500

    Config comments.

--- classes/Config.php
+++ classes/Config.php
@@ -1,19 +1,23 @@
 <?php
+    // Database connection information.
     define('DB_HOST', 'localhost');
     define('DB_NAME', 'zap-hostingesxpack_622951');
     define('DB_USER', 'root');
     define('DB_PASS', '');
 
+    // Search types.
     define('ENABLED_FIREARMS', false);
     define('ENABLED_CIVILIANS', true);
     define('ENABLED_VEHICLES', true);
 
+    // Table names within database.
     define('TABLE_CIVILIANS', 'users');
     define('TABLE_VEHICLES', 'owned_vehicles');
     define('TABLE_FIREARMS', 'firearms');
     define('TABLE_USERS', 'police_members');
     define('TABLE_BOLOS', 'spacecad_bolos');
 
+    // List all slogans that you'd like to show up at the top of the MDT.
     define('SLOGANS', [
         "Homicide: Our day starts when yours ends.",
         "Our Colors Never Run.",
